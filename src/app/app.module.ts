5791b5783f98b3a0c732735c6e5969e0ef6daab1
Maximilian Bruhn <maximilianbruhn@gmx.de>
2022-11-20 10:51:47 +0100

    first playable version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,38 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StartscreenComponent } from './startscreen/startscreen.component';
 import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import { GameInfoComponent } from './game-info/game-info.component';
+import {MatCardModule} from '@angular/material/card';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     StartscreenComponent,
-    GameComponent
+    GameComponent,
+    PlayerComponent,
+    DialogAddPlayerComponent,
+    GameInfoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
@@ -7,11 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  constructor() { }
+  currentCard: string = '';
+  game: Game;
+  constructor(public dialog: MatDialog) {
+    
+   }
 
   ngOnInit(): void {
+    this.newGame();
   }
   pickCard(){
-   this.pickCardAnimation = true;   
+    if (this.game.stack.length == 0){
+      this.endGame()
+         }
+    else{
+
+
+      if(!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();     // this.game.stack.pop() nimmt die letzte Karte vom Array Stack und löscht sie gleichzeitig.
+      this.pickCardAnimation = true;
+      
+      console.log(this.game)
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+      setTimeout(() => {
+        this.game.playedCard.push(this.currentCard)
+        this.pickCardAnimation = false;     // setzen Animation zurück, damit die Animation bei jeder Karte zurück gesetzt wird.
+      }, 1200);
+    }
+
+    
+    } 
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game)
+  }
+
+  endGame(){
+    console.log('das spiel ist zu ende')
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe(name => {
+      if(name && name.length > 0){  //prüfen ob es name überhaupt gibt, danach wird geprüft ob der Name länger als 0 ist
+      this.game.players.push(name);}
+    });
   }
 }
